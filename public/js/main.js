7defc5ab2748c5e600c1098099c6d4eee654feea
Roger Esteve <Coconaut7@gmail.com>
2018-12-06 00:25:08 +0100

    Removing config to hide api key :D

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,3 @@
-const config = {
-	apiKey: "AIzaSyBmKwsxDy0PNrZsTQUfHGQCM6BUADF90LI",
-	authDomain: "tr-db-24f7e.firebaseapp.com",
-	databaseURL: "https://tr-db-24f7e.firebaseio.com/",
-	projectId: "tr-db-24f7e",
-	storageBucket: "tr-db-24f7e.appspot.com",
-};
 firebase.initializeApp(config);
 
 const database = firebase.database();
